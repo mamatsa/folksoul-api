e320e6b7981425b5377ec2c23e2ce48c74c5c0dc
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-08-22 12:52:32 +0400

    feat: add member avatar upload endpoint

--- src/controllers/band-members-controller.ts
+++ src/controllers/band-members-controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express'
 import asyncHandler from 'express-async-handler'
 import { BandMember } from 'models'
 import { AddMemberBody } from 'types'
+import fs from 'fs/promises'
 
 // @desc     Get all band members
 // @route    GET /band-members
@@ -15,45 +16,65 @@ export const getMembers = asyncHandler(async (_: Request, res: Response) => {
 // @route    POST /band-member
 // @access   Private
 export const addMember = asyncHandler(async (req: Request, res: Response) => {
-  const { name, instrument, orbitWidth, color, bio } = req.body as AddMemberBody
-  if (!req.file) {
-    res.status(422)
-    throw new Error('no image provided.')
-  }
-  const avatarUrl: string = req.file!.path
-  const bandMember = await BandMember.create({
-    avatarUrl,
-    name,
-    instrument,
-    orbitWidth,
-    color,
-    bio,
-  })
-
-  res
-    .status(200)
-    .json({ status: 'success', data: { addedBandMember: bandMember } })
+  const addedBandMember = await BandMember.create(req.body as AddMemberBody)
+  res.status(200).json({ status: 'success', data: { addedBandMember } })
 })
+
+// @desc     Add band member avatar
+// @route    PUT /band-member/avatar
+// @access   Private
+export const addMemberAvatar = asyncHandler(
+  async (req: Request, res: Response) => {
+    // Check if image is provided
+    if (!req.file) {
+      res.status(422)
+      throw new Error('no image provided')
+    }
+
+    // Extract image path in filesystem
+    const avatarUrl: string = req.file!.path
+
+    // Find band member
+    const bandMember = await BandMember.findById(req.params.id)
+    if (!bandMember) {
+      res.status(400)
+      throw new Error('wrong member id')
+    }
+
+    // Update existing band member
+    const updatedBandMember = await bandMember.updateOne({
+      avatarUrl,
+    })
+
+    // Remove old image
+    if (updatedBandMember && bandMember.avatarUrl) {
+      await fs.unlink(bandMember.avatarUrl)
+    }
+
+    res.status(200).json({
+      status: 'success',
+      data: { avatarUrl },
+    })
+  }
+)
 
 // @desc     Update band member
 // @route    PUT /band-member
 // @access   Private
 export const updateMember = asyncHandler(
   async (req: Request, res: Response) => {
-    const updatedMember = await BandMember.findByIdAndUpdate(
+    const updatedBandMember = await BandMember.findByIdAndUpdate(
       req.params.id,
-      req.body,
+      req.body as AddMemberBody,
       {
         new: true,
       }
     )
-    if (!updatedMember) {
-      res.status(422)
+    if (!updatedBandMember) {
+      res.status(400)
       throw new Error('wrong member id')
     }
-    res
-      .status(200)
-      .json({ status: 'success', data: { updatedBandMember: updatedMember } })
+    res.status(200).json({ status: 'success', data: { updatedBandMember } })
   }
 )
 
@@ -64,7 +85,7 @@ export const deleteMember = asyncHandler(
   async (req: Request, res: Response) => {
     const bandMember = await BandMember.findById(req.params.id)
     if (!bandMember) {
-      res.status(422)
+      res.status(400)
       throw new Error('wrong band member id')
     }
     await bandMember.remove()
