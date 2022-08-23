84f1a95acbe3340a74621c43d4b006d348d12ef0
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-08-23 08:28:38 +0400

    feat: protect band member routes

--- src/routes/band-members.ts
+++ src/routes/band-members.ts
@@ -6,15 +6,25 @@ import {
   updateMember,
   deleteMember,
 } from 'controllers'
-import { multerMiddleware, validateBody } from 'middlewares'
+import { multerMiddleware, validateBody, protect } from 'middlewares'
 import { bandMemberSchema } from 'schemas'
 
 const router = Router()
 
-router.get('/band-members', getMembers)
-router.post('/band-member', validateBody(bandMemberSchema), addMember)
-router.put('/band-member/avatar/:id', multerMiddleware, addMemberAvatar)
-router.put('/band-member/:id', validateBody(bandMemberSchema), updateMember)
-router.delete('/band-member/:id', deleteMember)
+router.get('/band-members', protect, getMembers)
+router.post('/band-member', protect, validateBody(bandMemberSchema), addMember)
+router.put(
+  '/band-member/avatar/:id',
+  protect,
+  multerMiddleware,
+  addMemberAvatar
+)
+router.put(
+  '/band-member/:id',
+  protect,
+  validateBody(bandMemberSchema),
+  updateMember
+)
+router.delete('/band-member/:id', protect, deleteMember)
 
 export default router
