aeb5d5dacc424f98e9741905a1e656f96eb47bf8
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-08-31 14:04:51 +0400

    feat: add band schema

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -1,3 +1,4 @@
 export { default as loginSchema } from './login-schema'
 export { default as bandMemberSchema } from './band-members-schema'
 export { default as socialLinkSchema } from './social-links-schema'
+export { default as bandSchema } from './band-schema'
