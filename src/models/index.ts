4cc47d19da68e996c2532ff5414c822ab0a37af3
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-08-31 13:56:33 +0400

    feat: add band model

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,3 +1,4 @@
 export { default as Admin } from './Admin'
 export { default as BandMember } from './BandMember'
 export { default as SocialLink } from './SocialLink'
+export { default as Band } from './Band'
