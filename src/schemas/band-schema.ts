cb9c02cd880a25e23359c55ea4413ceb32b4a20c
otomamatsashvili <otar.mamatsashvili@gmail.com>
2022-08-31 21:48:22 +0400

    fix: remove about band field regex validation

--- src/schemas/band-schema.ts
+++ src/schemas/band-schema.ts
@@ -1,15 +1,10 @@
 import Joi from 'joi'
 
 const bandSchema = Joi.object({
-  about: Joi.string()
-    .required()
-    .pattern(new RegExp(/^[0-9ა-ჰ.:,!?"-\s]*$/))
-    .messages({
-      'any.required': 'about field is required',
-      'string.base': 'about field should be string',
-      'string.pattern.base':
-        'about should only contain georgian language characters',
-    }),
+  about: Joi.string().required().messages({
+    'any.required': 'about field is required',
+    'string.base': 'about field should be string',
+  }),
 })
 
 export default bandSchema
